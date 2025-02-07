437e040b737d8b28bbf2085e6187d61cc5db9ef6
Sebastian Wolf <sebastian@wolfware.app>
2025-02-07 20:54:19 +0100

    Change Version to 3.5.9.0

--- src/main/java/app/wolfware/Value.java
+++ src/main/java/app/wolfware/Value.java
@@ -5,6 +5,6 @@ class Value {
 	public final static int STANDART_SERVER_PORT = 1436;
 	public final static byte[] STANDART_KNOTEN_ANFANG = {0, 0, 0, 0};
 	public final static byte[] STANDART_KNOTEN_ENDE = {-1, -1, -1, -1};
-	public final static String VERSION = "3.5.6.0";
-	
+	public final static String VERSION = "3.5.9.0";
+
 }
